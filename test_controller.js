c38caeb247e684a1c1fe604e2a11cf1089d32f9a
root <srinivasrk>
2016-05-17 15:14:20 +0530

    changed a and b values

--- test_controller.js
+++ test_controller.js
@@ -2,10 +2,10 @@ var myApp = angular.module('myApp',[]);
 
 function test($scope){
 	$scope.a=10;
-	$scope.b=20;
+	$scope.b=220;
 }
 function test1($scope){
-	$scope.a=20;
+	$scope.a=120;
 	$scope.b=20;
 }
 myApp.controller('testcontroller',test);
